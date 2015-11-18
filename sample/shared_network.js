5df5b49518a0242346cf8c786a4c54e3921d60b4
Shuhei Hiya <syuhei176@gmail.com>
2015-11-19 02:34:20 +0900

    Renamed sample

--- sample/shared_network.js
+++ sample/shared_network.js
@@ -6,30 +6,15 @@ var sakuraCloud = new SakuraCloud({
 	accessTokenSecret  : process.env.SAKURA_SECRET
 });
 
-var server = sakuraCloud.server('aaa');
+var server = sakuraCloud.server('Test Server');
 server.create(function(err, result) {
 	console.log('create', err);
 	server.createInterface(function(err) {
 		server.sharedInterface(function(err) {
 			server.powerOn(function(err) {
 				console.log('power on', err);
-				server.powerOff(function(err, result) {
-					console.log('power off', err);
-					server.terminate(function(err, result) {
-						console.log(err, result);
-					});
-				});
 			});
 		});
 	});
 });
 
-/*
-client.createRequest({
-  method: 'GET',
-  path  : 'server'
-}).send(function(err, result) {
-  if (err) throw new Error(err);
-  console.log( result.response.servers.map(function(s) {return s.name;}) );
-});
-*/
